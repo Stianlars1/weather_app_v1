4cb9c08ca4b47b7bb761e986eb64718aaa586eaa
stian.larsen <stian.larsen@sparebank1.no>
2024-10-30 22:07:08 +0100

    Added Test setup | updated dev-dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest/config" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), basicSsl()],
+  build: {
+    outDir: "build",
+    sourcemap: false,
+    emptyOutDir: true,
+  },
+
+  server: {
+    host: true,
+    port: 3000,
+    https: {},
+    hmr: {
+      port: 3000,
+      host: "localhost",
+    },
+  },
+
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./src/test/setupTests.ts"],
+  },
+});

738afbe8f8c9f57e3831c8df4513c2d26662a1f8
stian.larsen <stian.larsen@sparebank1.no>
2024-10-31 01:44:10 +0100

    updated vite.config.ts to handle VERCEL deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,17 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import basicSsl from "@vitejs/plugin-basic-ssl";
-import fs from "fs";
-import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react()],
   build: {
     outDir: "build",
     sourcemap: false,
-    emptyOutDir: true,
   },
 
   server: {
     host: true,
     port: 3000,
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, "./cert/localhost-key.pem")),
-      cert: fs.readFileSync(path.resolve(__dirname, "./cert/localhost.pem")),
-    },
   },
 
   test: {

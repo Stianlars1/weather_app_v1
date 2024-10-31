49766f6c5a990d23a73250963fd4d794742e72f6
stian.larsen <stian.larsen@sparebank1.no>
2024-10-31 01:21:28 +0100

    Updated urls and vite.config.ts

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,3 +1,5 @@
 export const ROOT_PATH = "/";
 export const OPEN_WEATHER_URL =
   "https://api.openweathermap.org/data/2.5/weather";
+export const OPEN_WEATHER_ICON_URL = "http://openweathermap.org/img/w";
+export const OPEN_WEATHER_ICON_FORMAT = ".png";
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import basicSsl from "@vitejs/plugin-basic-ssl";
-
+import fs from "fs";
+import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), basicSsl()],
@@ -15,10 +16,9 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
-    https: {},
-    hmr: {
-      port: 3000,
-      host: "localhost",
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, "./cert/localhost-key.pem")),
+      cert: fs.readFileSync(path.resolve(__dirname, "./cert/localhost.pem")),
     },
   },
 
